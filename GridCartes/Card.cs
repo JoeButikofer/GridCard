6ee88ce6247f162420c725892d3b60ad7bd750cb
JoeButikofer <joebutikofer@gmail.com>
2015-04-06 17:13:35 +0200

    Ajout automatique des valeurs sur l'image de la carte

--- GridCartes/Card.cs
+++ GridCartes/Card.cs
@@ -70,10 +70,39 @@ namespace GridCartes
             level = _level;
 
             String path = Path.Combine(
-        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
-        _imagePath);
-            Console.Write(path);
-            image = Image.FromFile(path);
+        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), _imagePath);
+
+            Image baseImage = Image.FromFile(path);
+
+            image = addValuesToImage(baseImage);
+        }
+
+        //Add damage value to the base image of the card, so we don't have to modify the image every time we chnage the values
+        private Image addValuesToImage(Image img)
+        {
+            //we need to create a non indexed image so we can modify it
+            Bitmap newBitmap = new Bitmap(img.Width, img.Height);
+            Graphics graphics = Graphics.FromImage(newBitmap);
+            graphics.DrawImage(img, 0, 0);
+            Font font = new Font("Arial", 100);
+            Brush brush = Brushes.Black;
+
+            String text = ""+this.valueTop;
+            //Since we have numbers, the text size will not change too much
+            SizeF textSize = graphics.MeasureString(text, font);
+
+            graphics.DrawString(text, font, brush, img.Width/2 - textSize.Width/2, 0);
+
+            text = "" + this.valueLeft;
+            graphics.DrawString("" + this.valueLeft, font, brush, 0, img.Height / 2-textSize.Height/2);
+
+            text = "" + this.valueRight;
+            graphics.DrawString("" + this.valueRight, font, brush, img.Width - textSize.Width, img.Height / 2 - textSize.Height / 2);
+
+            text = "" + this.valueBottom;
+            graphics.DrawString("" + this.valueBottom, font, brush, img.Width / 2 - textSize.Width / 2, img.Height - textSize.Height);
+
+            return newBitmap;
         }
 
     }
