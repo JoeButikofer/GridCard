98242a7af0601a252fbe58cf1f5e895726124856
Spike1953 <kevin.baland@he-arc.ch>
2015-04-13 14:18:49 +0200

    GameBoard

--- GridCartes/DeckChoice.cs
+++ GridCartes/DeckChoice.cs
@@ -2,29 +2,53 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace GridCartes
 {
     public partial class DeckChoice : Form
     {
-        private string namePlayer;
+        private Player player;
+        private DatabaseHelper db;
 
         public DeckChoice(string name)
         {
             InitializeComponent();
-            namePlayer = name;
+            player = new Player(name);
+            db = DatabaseHelper.Instance;
 
-
+            initListBox();
         }
 
-        private void btn_Create_Click(object sender, EventArgs e)
+        private void initListBox()
         {
+            string sql = "select Nom from Decks where ID_Joueurs = '" + player.Id + "' ;";
+            SQLiteDataReader reader = db.execCommandeReader(sql);
 
+            while (reader.Read())
+            {
+                listBoxDeckChoice.Items.Add("" + reader["Nom"]);
+            }
+
+        }
+        private void DeckChoice_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Ferme tout le programme
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void listBoxDeckChoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Deck deck = new Deck(player.Id, listBoxDeckChoice.SelectedItem.ToString());
+            player.CurrentDeck = deck;
+            (new GameBoard(player)).Show();
+            this.Hide();
         }
 
     }
---
+++ GridCartes/GameBoard.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridCartes
+{
+    public partial class GameBoard : Form
+    {
+        private Player player;
+        private Deck currentDeck;
+        private Card selectedCard;
+
+        public GameBoard(Player p)
+        {
+            InitializeComponent();
+            player = p;
+            currentDeck = player.CurrentDeck;
+
+            /*TextBox text = new TextBox();
+            text.Text = "test1";
+
+            Panel panel = new Panel();
+            panel.BackColor = Color.Yellow;
+
+            tableLayoutPanel1.Controls.Add(panel, 0, 1);
+            tableLayoutPanel1.Controls.Add(text, 1, 0);*/
+
+            fillHandCards();
+        }
+
+
+        private void fillHandCards()
+        {
+            listViewHandCards.Clear();
+            ImageList imageList = new ImageList();
+            imageList.ImageSize = new Size(50, 80);
+            foreach (Card card in currentDeck.ListCard)
+            {
+                imageList.Images.Add(card.Name, card.Image);
+                var listViewItem = listViewHandCards.Items.Add(card.Name);
+                listViewItem.ImageKey = card.Name;
+            }
+            listViewHandCards.LargeImageList = imageList;
+        }
+
+        private void listViewHandCards_ItemActivate(object sender, EventArgs e)
+        {
+            int i = listViewHandCards.SelectedIndices[0];
+            string s = listViewHandCards.Items[i].Text;
+
+            selectedCard = currentDeck.ListCard.ElementAt(i);
+
+        }
+
+        private void tableLayoutPanel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(selectedCard!=null)
+            {
+                int width = (int)(tableLayoutPanel1.Size.Width / 4);
+                int height = (int)(tableLayoutPanel1.Size.Height / 4);
+
+
+                int resteX = e.X % width;
+                int resteY = e.Y % height;
+
+                int x = (e.X - resteX) / width;
+                int y = (e.Y - resteY) / height;
+
+                Panel p = new Panel();
+                p.BackColor = Color.Red;
+
+                Image img = (Image)(new Bitmap(selectedCard.Image, new Size(width-4, height-4)));
+
+
+                p.BackgroundImage = img;
+                p.Visible = false;
+
+                tableLayoutPanel1.Controls.Add(p, x, y);
+
+                p.Visible = true;
+
+                currentDeck.removeCard(selectedCard);
+                fillHandCards();
+                selectedCard = null;
+            }
+            Console.WriteLine("qidhipozahdazhdipazgdfazodpigavdb");
+          
+
+        }
+
+        private void GameBoard_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+    }
+}
