28d7b7b893c497216f139c90590ddac9a7c092df
JoeButikofer <joebutikofer@gmail.com>
2015-04-24 14:51:31 +0200

    Added game server

---
+++ gameServer/gameServer/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace gameServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(38, 61);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(67, 38);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(135, 61);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(67, 38);
+            this.btn_Stop.TabIndex = 1;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 159);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+    }
+}
+
---
+++ gameServer/gameServer/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gameServer
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener tcpListener;
+        private bool started;
+        private Queue<String> listWaitingClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.btn_Stop.Enabled = false;
+            this.started = false;
+            //FIFO
+            this.listWaitingClient = new Queue<String>();
+            //TODO changer le loopback
+            tcpListener = new TcpListener(IPAddress.Loopback, 8012);
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            changeState();
+            tcpListener.Start();
+            Task task = HandleConnectionsAsync(tcpListener);
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            changeState();
+            tcpListener.Stop();
+        }
+
+        private void changeState()
+        {
+            started = !started;
+            this.btn_Stop.Enabled = !this.btn_Stop.Enabled;
+            this.btn_Start.Enabled = !this.btn_Start.Enabled;
+        }
+
+        async Task HandleConnectionsAsync(TcpListener listener)
+        {
+            while (started)
+            {
+                Console.Write("Waiting for async connection...");
+                Task<TcpClient> acceptClient = listener.AcceptTcpClientAsync();
+                TcpClient tcpClient = await acceptClient;
+                sendOpponentAddress(tcpClient);
+                Console.WriteLine("OK");
+                tcpClient.Close();
+            }
+        }
+
+        private void sendOpponentAddress(TcpClient tcpClient)
+        {
+            Stream stream = tcpClient.GetStream();
+
+            ASCIIEncoding encode = new ASCIIEncoding();
+            String address = ((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address.ToString();
+
+            String message = "";
+            //If nobody is available for a match, we put the client in the waiting list
+            if(listWaitingClient.Count <= 0)
+            {
+                listWaitingClient.Enqueue(address);
+                //and we say that he is the server
+                message = "SERVER";
+            }
+            //else, we take the first client in the list and we send his address
+            else
+            {
+                message = listWaitingClient.Dequeue();
+            }
+            byte[] byteMessage = encode.GetBytes(message);
+            Console.WriteLine("Send : " + message + "To : " + address);
+            stream.Write(byteMessage, 0, byteMessage.Length);
+            stream.Close();
+        }
+
+    }
+}
